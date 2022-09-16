220e908ab2a5c20c98539d95dbdaebfe86485b74
phot1 <phot1@DESKTOP-BG9GK3F>
2022-09-17 00:52:48 +0900

    commit

---
+++ ToyProject/src/main/java/DTOFolder/OrderItemName.java
@@ -0,0 +1,27 @@
+package DTOFolder;
+
+public class OrderItemName {
+
+		private String clothes;
+		private String pants;
+		private String card;
+		public String getClothes() {
+			return clothes;
+		}
+		public void setClothes(String clothes) {
+			this.clothes = clothes;
+		}
+		public String getPants() {
+			return pants;
+		}
+		public void setPants(String pants) {
+			this.pants = pants;
+		}
+		public String getCard() {
+			return card;
+		}
+		public void setCard(String card) {
+			this.card = card;
+		}
+		
+}
